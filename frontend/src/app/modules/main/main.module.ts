463f1cf208652095f50f769adb436a91664048fc
bartoszborys <borysbartosz1@gmail.com>
2019-11-09 18:08:05 +0100

    add angular materials to frontend

--- frontend/src/app/modules/main/main.module.ts
+++ frontend/src/app/modules/main/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
+import { MaterialsModule } from '../materials/materials.module';
 
 
 
@@ -9,7 +10,8 @@ import { MainRoutingModule } from './main-routing.module';
   declarations: [MainComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MaterialsModule,
   ]
 })
 export class MainModule { }
--- frontend/src/app/modules/sign/sign.module.ts
+++ frontend/src/app/modules/sign/sign.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign.component';
 import { SignRoutingModule } from './sign-routing.module'
+import { MaterialsModule } from '../materials/materials.module';
 
 
 @NgModule({
   declarations: [SignInComponent],
   imports: [
     CommonModule,
-    SignRoutingModule
+    SignRoutingModule,
+    MaterialsModule,
   ]
 })
 export class SignModule { }
