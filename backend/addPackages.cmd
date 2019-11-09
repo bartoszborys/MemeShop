eefddb798ee80178dbeeffc55bd7d74d03326f0a
bartoszborys <borysbartosz1@gmail.com>
2019-11-09 18:49:30 +0100

    add file for auto packages install

---
+++ backend/addPackages.cmd
@@ -0,0 +1,7 @@
+pip install django
+pip install django-cors-headers
+pip install mysqlclient
+pip install jwt
+pip install djangorestframework
+pip install djangorestframework-jwt
+pip install django-rest-swagger
