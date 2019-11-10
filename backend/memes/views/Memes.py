62f3638f668457eb6d79f8dca7349b833df81861
magdbaj160 <mb96@onet.eu>
2019-11-10 12:21:45 +0100

    get memes from database

--- backend/memes/views/Memes.py
+++ backend/memes/views/Memes.py
@@ -1,9 +1,15 @@
 from django.http import JsonResponse
+from ..models.Meme import Meme
+from ..serializers.MemesSerializer import MemesSerializer
 from rest_framework.generics import GenericAPIView
 
 
 class MemesView(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
-        return JsonResponse({"id":"abc"})
+        memes = Meme.objects.all()
+        serializer = MemesSerializer(memes, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
+    def post(self, request, *args, **kwargs):
+        
