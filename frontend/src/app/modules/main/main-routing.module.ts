eb04fdb541bb7bf6d56ec432bb3dd50d7335f92d
bartoszborys <borysbartosz1@gmail.com>
2019-11-10 13:29:23 +0100

    add memes routes latest/memes

--- frontend/src/app/modules/main/main-routing.module.ts
+++ frontend/src/app/modules/main/main-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
+import { MemesListComponent } from './components/memes-list/memes-list.component';
+import { LatestMemesComponent } from './components/latest-memes/latest-memes.component';
 
 const routes: Routes = [
   {
     path: "",
+    component: MainComponent,
     children: [
       {
-        path: "",
-        component: MainComponent
+        path: "memes",
+        component: MemesListComponent
+      },
+      {
+        path: "latest",
+        component: LatestMemesComponent
       }
     ]
   }
--- frontend/src/app/modules/main/main.module.ts
+++ frontend/src/app/modules/main/main.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
 import { MaterialsModule } from '../materials/materials.module';
+import { MemesListComponent } from './components/memes-list/memes-list.component';
+import { LatestMemesComponent } from './components/latest-memes/latest-memes.component';
 
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MemesListComponent, LatestMemesComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
