71b3252d77841a5e2a97dc247b62e9dbd021b0c6
bartoszborys <borysbartosz1@gmail.com>
2019-11-22 07:47:12 +0100

    now backend is data source for memes-list and one-meme-view

--- frontend/src/app/modules/main/services/memes/memes.service.ts
+++ frontend/src/app/modules/main/services/memes/memes.service.ts
@@ -1,32 +1,22 @@
 import { Injectable } from '@angular/core';
-import memesListMock from './memes.mock.database';
 import { of, Observable } from 'rxjs';
 import { delay } from 'rxjs/operators'; 
 import { MemeCard } from '../../components/memes-list/models/meme-card.model';
 import { MemeDetails } from '../../components/meme/models/meme-details.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getMemesList(): Observable<MemeCard[]>{
-    return of(memesListMock).pipe( delay(200) )
+    return this.http.get<MemeCard[]>(`http://localhost:8000/api/memes/`);
   }
 
   getMeme(id: number): Observable<MemeDetails>{
-    const mappedMeme: MemeDetails = memesListMock.filter(meme => meme.id == id).map( (meme: MemeCard): MemeDetails => {
-      return {
-        id: meme.id,
-        name: meme.title,
-        author: meme.type,
-        useCount: 5,
-        image: meme.image
-      }
-    })[0];
-
-    return of(mappedMeme).pipe( delay(200));
+    return this.http.get<MemeDetails>(`http://localhost:8000/api/memes/${id}`);
   }
 }
