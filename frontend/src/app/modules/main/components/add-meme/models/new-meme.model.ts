7788ddfd50f3d415dcdf0f6fd3073b9a84c35a11
bartoszborys <borysbartosz1@gmail.com>
2019-11-25 17:08:37 +0100

    add `add-meme` section with backend request.

---
+++ frontend/src/app/modules/main/components/add-meme/models/new-meme.model.ts
@@ -0,0 +1,6 @@
+export interface NewMeme {
+  title: string;
+  blob: string | ArrayBuffer;
+  price: number;
+  quantity: number;
+}
--- frontend/src/app/modules/main/services/memes/memes.service.ts
+++ frontend/src/app/modules/main/services/memes/memes.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
-import { delay } from 'rxjs/operators'; 
+import { Observable } from 'rxjs';
 import { MemeCard } from '../../components/memes-list/models/meme-card.model';
 import { MemeDetails } from '../../components/meme/models/meme-details.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NewMeme } from '../../components/add-meme/models/new-meme.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,20 @@ export class MemesService {
 
   constructor(private http: HttpClient) { }
 
-  getMemesList(): Observable<MemeCard[]>{
+  getMemesList(): Observable<MemeCard[]> {
     return this.http.get<MemeCard[]>(`http://localhost:8000/api/memes/`);
   }
 
-  getMeme(id: number): Observable<MemeDetails>{
+  getMeme(id: number): Observable<MemeDetails> {
     return this.http.get<MemeDetails>(`http://localhost:8000/api/memes/${id}`);
+  }
+
+  createMeme(newMeme: NewMeme): Observable<NewMeme> {
+    return this.http.post<NewMeme>(`http://localhost:8000/api/memes/`, newMeme, {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': `bearer ${localStorage.getItem('auth_token')}`,
+      })
+    })
   }
 }
