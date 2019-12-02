f5b16abd789f0f79fad9c8b9d022267f3d438584
DESKTOP-ASA2QOQ\Monia <monikakokot96@gmail.com>
2019-12-02 22:26:57 +0100

    add getting images by url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,8 @@ from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 from rest_framework_swagger.views import get_swagger_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_swagger_view(title='Meme Shop API')
 
@@ -18,3 +20,4 @@ urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^$', schema_view)
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
