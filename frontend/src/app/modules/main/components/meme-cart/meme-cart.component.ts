f23fb7768f1621eb40dfca3bbcba1a73cd328e52
bartoszborys <borysbartosz1@gmail.com>
2019-12-07 22:31:27 +0100

    cart table is sortable

--- frontend/src/app/modules/main/components/meme-cart/meme-cart.component.ts
+++ frontend/src/app/modules/main/components/meme-cart/meme-cart.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Sort } from '@angular/material/sort';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { ActivatedRoute } from '@angular/router';
 import { MemeDetails } from '../../models/meme-details.model';
 import { Observable } from 'rxjs';
 import { MemesCartService } from '../../services/memes-cart/memes-cart.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -13,6 +14,10 @@ import { MemesCartService } from '../../services/memes-cart/memes-cart.service';
 })
 export class MemeCartComponent implements OnInit {
   public cartMemesOrigin: MemeDetails[] = [];
+  public dataSource: MatTableDataSource<MemeDetails> | null = null;
+  
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
 
   public displayedColumns = ['src', 'name', 'price', 'quantity', 'bought', 'cart', 'author', 'remove'];
 
@@ -23,19 +28,23 @@ export class MemeCartComponent implements OnInit {
 
   public async ngOnInit(): Promise<void> {
     this.cartMemesOrigin = await (<Observable<MemeDetails[]>>this.route.snapshot.data.cart).toPromise();
+    this.dataSource = new MatTableDataSource<MemeDetails>(this.cartMemesOrigin);
+    this.dataSource.sort = this.sort;
   }
 
   get totalPrice(): number {
     if (this.cartMemesOrigin.length <= 0) {
       return 0;
     }
-    return this.cartMemesOrigin.map(item => item.price * this.inCartCount(item.id)).reduce((prevValue, currentValue) => prevValue + currentValue);
+    return this.cartMemesOrigin
+      .map(item => item.price * this.inCartCount(item.id))
+      .reduce((prevValue, currentValue) => prevValue + currentValue);
   }
 
   public removeFromCart(id: number): void {
     this.cart.remove(id);
 
-    if( this.inCartCount(id) <= 0 ) {
+    if ( this.inCartCount(id) <= 0 ) {
       this.cartMemesOrigin = this.cartMemesOrigin.filter(item => item.id != id);
     }
   }
